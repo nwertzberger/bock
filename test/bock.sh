71de488c6f3de82f018584a85ac750b69be3a387
Nicholas Wertzberger <wertnick@gmail.com>
2013-02-23 13:33:01 -0600

    Cleaned up tests.

--- test/bock.sh
+++ test/bock.sh
@@ -4,19 +4,19 @@
 source ./bock thisCall
 echo -n "Testing if the last call is being kept track of..."
 thisCall
-if verify thisCall
+if verify thisCall 2>/dev/null
 then
     echo PASSED
 else
     echo FAILED
 fi
 
-echo "Testing if resetMocks clears out all mocks"
+echo -n "Testing if resetMocks clears out all mocks..."
 resetMocks thisCall
 
-verify thisCall && echo FAILED || echo PASSED
+verify thisCall 2>/dev/null && echo FAILED || echo PASSED
 
-echo "Testing if these mocks are carried into a sub-program"
+echo -n "Testing if these mocks are carried into a sub-program..."
 
 source ./bock mv
 . test/testScript
